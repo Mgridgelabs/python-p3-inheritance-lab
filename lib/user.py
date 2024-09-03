809bd8ab221f4bfddb7a4a03c4157911e271d37d
Mgridgelabs <mainaianndungu@gmail.com>
2024-09-04 00:21:35 +0300

    completes inheritance lab1

--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
 
 class User:
-    pass
+    def __init__(self,first_name=None,last_name=None):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    # def get_full_name(self):
+    #     return f"{self.first_name} {self.last_name}"
+    
+    # def set_full_name(self):
+    #     self.first_name, self.last_name = self.get_full_name().split()
+    
